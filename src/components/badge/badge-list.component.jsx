a716a6c829fba6edd79d92216cae37438185651f
David Sljukic <david.sljukic.dev@gmail.com>
2021-10-25 23:50:45 +0200

    chore: revert badges list since it's unneceessary change

--- src/components/badge/badge-list.component.jsx
+++ src/components/badge/badge-list.component.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
+// libs
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const BadgeList = () => {
-  return <div className='badge-list'>BADGES LIST</div>;
+// styles
+import './badges.styles.scss';
+
+// firestore
+import { fetchAllBadges } from '../../firebase/firebase-fetch';
+
+// components
+import Badge from './badge.component';
+
+// redux
+import { setBadges } from '../../redux/badges/badges.actions';
+import { getBadges } from '../../redux/badges/badges.selectors';
+
+// utils
+import { renderPlaceholders } from '../../utils';
+
+// constants
+const PLACEHOLDER_NUMBER = 8;
+
+const renderData = (badges, showActions) => {
+  return badges.map(badge => {
+    return <Badge key={badge.id} badge={badge} showActions={showActions} />;
+  });
+};
+//
+
+// component
+const BadgeList = ({ showActions }) => {
+  const dispatch = useDispatch();
+  const badgesList = useSelector(getBadges);
+
+  useEffect(() => {
+    (async () => {
+      if (!badgesList) {
+        const badgesData = await fetchAllBadges();
+        dispatch(setBadges(badgesData));
+      }
+    })();
+  }, [dispatch]);
+
+  return (
+    <div className='badge-list'>
+      {!badgesList
+        ? renderPlaceholders(PLACEHOLDER_NUMBER)
+        : renderData(badgesList, showActions)}
+    </div>
+  );
 };
 
 export default BadgeList;
